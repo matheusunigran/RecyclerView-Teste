39fc2a2a300039c5ec21424582e290928c405aa1
andreunigran <andre.martins@unigran.br>
2022-08-24 12:02:36 -0400

    Criação da tela

--- app/src/main/java/br/unigran/agenda/MainActivity.java
+++ app/src/main/java/br/unigran/agenda/MainActivity.java
@@ -3,12 +3,23 @@ package br.unigran.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nome;
+    EditText telefone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nome=findViewById(R.id.nomeId);
+        telefone=findViewById(R.id.telefoneId);
     }
+    public void salvar(View view){
+
+    }
+
+
 }
