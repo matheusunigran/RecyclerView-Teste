82e0481b9bc5817e8c5bf67afede82a1f6958534
andreunigran <andre.martins@unigran.br>
2022-08-25 01:00:33 -0400

    criado função de salvar

--- app/src/main/java/br/unigran/agenda/MainActivity.java
+++ app/src/main/java/br/unigran/agenda/MainActivity.java
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nome;
     EditText telefone;
+    List<Contato> dados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nome=findViewById(R.id.nomeId);
         telefone=findViewById(R.id.telefoneId);
+        dados= new ArrayList();
     }
     public void salvar(View view){
-
+        Contato contato = new Contato();
+        contato.setNome(nome.getText().toString());
+        contato.setTelefone(telefone.getText().toString());
+        dados.add(contato);
+        Toast.makeText(this,"Salvo com sucesso",Toast.LENGTH_SHORT).show();
     }
 
 
